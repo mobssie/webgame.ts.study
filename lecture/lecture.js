9004122b93975d20f6e28aec33de45584cf76350
mobssie <mobssie.jung@gmail.com>
2021-05-22 18:19:09 +0900

    typescript webgame 1step

--- lecture/lecture.js
+++ lecture/lecture.js
@@ -1,1 +1,36 @@
-console.log('hello js')
+var numberOne = Math.ceil(Math.random() * 9);
+var numberTwo = Math.ceil(Math.random() * 9);
+var result = numberOne * numberTwo;
+var string = 'ddd';
+var boolean = true;
+var wordNumber = document.createElement('div');
+wordNumber.textContent = numberOne + " * \uACF1\uD558\uAE30 " + numberTwo + "\uB294?";
+document.body.append(wordNumber);
+var form = document.createElement('form');
+var hello = document.body.append(form);
+var input = document.createElement('input');
+input.type = 'number';
+form.append(input);
+var button = document.createElement('button');
+button.textContent = '입력';
+form.append(button);
+var resultDiv = document.createElement('div');
+document.body.append(resultDiv);
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (result === Number(input.value)) {
+        resultDiv.textContent = '딩동댕';
+        numberOne = Math.ceil(Math.random() * 9);
+        numberTwo = Math.ceil(Math.random() * 9);
+        result = numberOne * numberTwo;
+        wordNumber.textContent = numberOne + " \uACF1\uD558\uAE30 " + numberTwo + "\uB294?";
+        input.value = '';
+        input.focus();
+    }
+    else {
+        resultDiv.textContent = '땡';
+        input.value = '';
+        input.minLength = 3;
+        input.focus();
+    }
+});
